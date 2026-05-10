fc62dd0386a02ddfeb0ab17edafe429a2b3b94f4
Enric-xX <almenarenric4@gmail.com>
2026-05-10 14:56:57 +0200

    Create Shaders-caché.bat for system optimization

---
+++ Shaders-caché.bat
@@ -0,0 +1,26 @@
+@echo off
+title CoreOptimizer v1.0 - CoreDev-HUB
+color 0b
+
+echo ==========================================
+echo       COREDEV-HUB | SYSTEM OPTIMIZER
+echo ==========================================
+echo.
+
+del /s /f /q %temp%\*.* >nul 2>&1
+rd /s /q %temp% >nul 2>&1
+mkdir %temp%
+
+del /s /f /q C:\Windows\Prefetch\*.* >nul 2>&1
+
+ipconfig /flushdns >nul
+
+netsh int tcp set global autotuninglevel=normal >nul
+
+echo.
+echo ==========================================
+echo    SISTEMA OPTIMIZADO
+echo ==========================================
+echo.
+pause
+exit
